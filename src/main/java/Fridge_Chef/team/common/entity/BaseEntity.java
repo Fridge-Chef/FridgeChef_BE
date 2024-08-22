b372963767904b56547bb5e17e2aa30083a78b7e
Kang JeoungHun <jeonghun.kang.dev@gmail.com>
2024-08-23 06:44:27 +0900

    JPA 추가  (#13)
    * :sparkles: jpa , querydsl 의존 주입
    
    * :sparkles: 베이스 타입 추가
    
    * :sparkles: 로컬,운영 환경 db 추가
    
    * :sparkles: 레시피 도메인 추가

---
+++ src/main/java/Fridge_Chef/team/common/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package Fridge_Chef.team.common.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BaseEntity {
+
+    @Column(name = "create_time")
+    @CreatedDate
+    private LocalDateTime createTime;
+    @Column(name = "update_time")
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+    private OracleBoolean isDelete;
+}
