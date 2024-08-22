7555e7a9ab454e2e6cbc675b7f153368893d1453
oineh <jeonghun.kang.dev@gmail.com>
2024-08-23 03:05:28 +0900

    :recycle: https config 환경에 맞게 분리

--- src/main/java/Fridge_Chef/team/config/webhttps/LocalConfig.java
+++ src/main/java/Fridge_Chef/team/config/webhttps/LocalConfig.java
@@ -1,18 +1,29 @@
-package Fridge_Chef.team.config;
+package Fridge_Chef.team.config.webhttps;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
-public class HttpsConfig implements WebMvcConfigurer {
+@Profile("local")
+public class LocalConfig implements WebMvcConfigurer {
+
+    @Value("${ssl.ip}")
+    private String ip;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedOrigins = ip.split(","); //ex) 127.0.0.1,127.0.0.2
+
         registry.addMapping("/**")
-                .allowedOrigins("*")  // 모든 도메인 허용  ex) .allowedOrigins("http://192.168.0.1", "http://example.com", "http://another-domain.com")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")  // 모든 HTTP 메서드 허용
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 자격 증명 허용
     }
 }
+
+
