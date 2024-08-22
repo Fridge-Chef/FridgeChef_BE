b25d14922def75545e66aa7c33b14a559f93f6f6
oineh <jeonghun.kang.dev@gmail.com>
2024-08-23 06:42:29 +0900

    :sparkles: 베이스 타입 추가

---
+++ src/main/java/Fridge_Chef/team/common/entity/OracleBoolean.java
@@ -0,0 +1,23 @@
+package Fridge_Chef.team.common.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Getter
+@RequiredArgsConstructor
+@Accessors(fluent = true)
+public enum OracleBoolean {
+    T(true),
+    F(false);
+    private final boolean bool;
+
+    public static OracleBoolean of(boolean bool){
+        if(bool) return T;
+        return F;
+    }
+
+    public int jdbcInsert(){
+        return this == T ? 1 : 0;
+    }
+}
