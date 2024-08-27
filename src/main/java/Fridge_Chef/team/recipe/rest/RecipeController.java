ac583a8baea993137c55ad60a3a05d4c4f6f7351
JHKoder <jeonghun.kang.dev@gmail.com>
2024-08-27 21:41:17 +0900

    :sparkles: 레시피 임시 api 추가

--- src/main/java/Fridge_Chef/team/recipe/rest/RecipeController.java
+++ src/main/java/Fridge_Chef/team/recipe/rest/RecipeController.java
@@ -1,18 +1,26 @@
 package Fridge_Chef.team.recipe.rest;
 
+import Fridge_Chef.team.recipe.rest.request.SampleRequest;
+import Fridge_Chef.team.recipe.rest.response.SampleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class RecipeController {
 
-    @GetMapping("")
+    @GetMapping()
     public String info(){
         return "web request success";
     }
+
+    @PostMapping("/test")
+    public SampleResponse docs(@RequestBody SampleRequest dto){
+        return new SampleResponse(dto.getName(),dto.getValue());
+    }
+
 }
