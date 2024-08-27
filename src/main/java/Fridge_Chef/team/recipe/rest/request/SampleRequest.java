37e751714905f942206bf5ba998cab3d164c91db
JHKoder <jeonghun.kang.dev@gmail.com>
2024-08-27 21:41:00 +0900

    :sparkles: api 테스트 샘플 추가

---
+++ src/main/java/Fridge_Chef/team/recipe/rest/request/SampleRequest.java
@@ -0,0 +1,13 @@
+package Fridge_Chef.team.recipe.rest.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SampleRequest {
+    String name;
+    String value;
+}
---
+++ src/main/java/Fridge_Chef/team/recipe/rest/response/SampleResponse.java
@@ -0,0 +1,13 @@
+package Fridge_Chef.team.recipe.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleResponse {
+    String name;
+    String value;
+}
