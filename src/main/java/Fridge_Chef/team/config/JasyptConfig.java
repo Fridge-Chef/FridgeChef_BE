5dd60e1931e7a2ef02addb15c94f112f18ad8383
JHKoder <jeonghun.kang.dev@gmail.com>
2024-08-27 22:21:16 +0900

    :bug: cicd 환경변수 셋팅 수정

--- src/main/java/Fridge_Chef/team/config/JasyptConfig.java
+++ src/main/java/Fridge_Chef/team/config/JasyptConfig.java
@@ -6,9 +6,9 @@ import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class JasyptConfig {
 
     @Value("${jasypt.encryptor.password}")
@@ -29,6 +29,7 @@ public class JasyptConfig {
         config.setKeyObtentionIterations("1000");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
         encryptor.setConfig(config);
+
         return encryptor;
     }
 }
