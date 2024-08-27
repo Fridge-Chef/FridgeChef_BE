d00d0d6c6244be6911abac536db80728c5f277d8
JHKoder <jeonghun.kang.dev@gmail.com>
2024-08-28 03:48:12 +0900

    :recycle: ci/cd  값 확인

--- src/test/java/Fridge_Chef/team/TeamApplicationTests.java
+++ src/test/java/Fridge_Chef/team/TeamApplicationTests.java
@@ -1,13 +1,18 @@
 package Fridge_Chef.team;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TeamApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+    @Test
+    void contextLoads() {
+        System.out.println("password: "+password);
+
+    }
 
 }
