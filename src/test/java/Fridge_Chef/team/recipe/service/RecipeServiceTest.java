d038167caf255b9cb86d95926165c953ecbff7ca
JHKoder <jeonghun.kang.dev@gmail.com>
2024-08-28 22:10:39 +0900

    :recycle: yml 수정

--- src/test/java/Fridge_Chef/team/recipe/service/RecipeServiceTest.java
+++ src/test/java/Fridge_Chef/team/recipe/service/RecipeServiceTest.java
@@ -14,8 +14,6 @@ public class RecipeServiceTest {
     @Test
     public void save() {
 
-        String password = System.getenv("JASYPT_PASSWORD");
-        System.out.println("password:"+password);
         String str = "새우두부계란찜\n연두부 75g(3/4모), 칵테일새우 20g(5마리), 달걀 30g(1/2개), 생크림 13g(1큰술), 설탕 5g(1작은술), 무염버터 5g(1작은술)\n고명\n시금치 10g(3줄기)";
 
         // 예외 단어 목록
