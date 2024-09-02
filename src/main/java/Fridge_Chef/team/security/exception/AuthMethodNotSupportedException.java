6b3c815d268ccc127581aa70834f0c41d5c544f7
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-02 19:44:09 +0900

    :sparkles: add jwt Exception

---
+++ src/main/java/Fridge_Chef/team/security/exception/AuthMethodNotSupportedException.java
@@ -0,0 +1,9 @@
+package Fridge_Chef.team.security.exception;
+
+import org.springframework.security.authentication.AuthenticationServiceException;
+
+public class AuthMethodNotSupportedException extends AuthenticationServiceException {
+    public AuthMethodNotSupportedException(String msg) {
+        super(msg);
+    }
+}
