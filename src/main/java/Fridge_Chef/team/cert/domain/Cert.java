129b18273522b8b0a59004881f171c73c339b006
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-02 22:13:27 +0900

    :sparkles: add 이메일 인증 도메인 추가

---
+++ src/main/java/Fridge_Chef/team/cert/domain/Cert.java
@@ -0,0 +1,29 @@
+package Fridge_Chef.team.cert.domain;
+
+import Fridge_Chef.team.common.entity.BaseEntity;
+import Fridge_Chef.team.common.entity.OracleBoolean;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Cert extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private int verificationCode;
+    private String email;
+    @Enumerated(value = EnumType.STRING)
+    private OracleBoolean authentication;
+
+    public Cert(int verificationCode, String email, OracleBoolean authentication) {
+        this.verificationCode = verificationCode;
+        this.email = email;
+        this.authentication=authentication;
+    }
+}
