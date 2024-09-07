ee7d6e95014a8985c178a6afe5849e37e87ad64e
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-08 02:24:50 +0900

    :white_check_mark: 유저 id type UUID 으로 변경 및 추가 정보는 Profile 객체로 대처

---
+++ src/main/java/Fridge_Chef/team/user/domain/Profile.java
@@ -0,0 +1,14 @@
+package Fridge_Chef.team.user.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+    private String username;
+}
