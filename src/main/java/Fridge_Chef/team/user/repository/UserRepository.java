4e4a25bd483b62defe1096e5832c1ecde83cfd20
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-08 03:47:48 +0900

    :sparkles: 유저 repository 추가

--- src/main/java/Fridge_Chef/team/user/repository/UserRepository.java
+++ src/main/java/Fridge_Chef/team/user/repository/UserRepository.java
@@ -5,10 +5,11 @@ import Fridge_Chef.team.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUserId(String userId);
+    Optional<User> findByUserId_Value(UUID userId);
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
