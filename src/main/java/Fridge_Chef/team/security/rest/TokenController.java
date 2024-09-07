a81b687508b263ace7c2a0874535ef68faefebe3
Kang JeoungHun <jeonghun.kang.dev@gmail.com>
2024-09-07 21:10:26 +0900

    ♻️  spring security jwt 셋팅 설정을 oauth2으로 변경 (#65)
    * :sparkles: spring security jwt oauth2 update
    
    * :sparkles: add h2.path local,dev
    
    * :fire: 사용되지 않는 security 설정 파일 제거
    -> oauth2 방식으로 변경됨
    
    * :fire: 사용되지 않는 security 설정 파일 제거
    -> oauth2 방식으로 변경됨
    
    * :recycle: add jwt Provider
    
    * :recycle: add token api
    
    * :recycle: refactor security config
    (oauth2 config)
    
    * :fire: remove security config
    (oauth2 config)
    
    * :recycle: add passwordEncoder config
    
    * :sparkles: update error_code
    password,token
    
    * :sparkles: api 에서 받을 공통 유저 토큰 -> 추출된 유저 UUID 값
    
    * :white_check_mark: jwt provider test
    
    * :white_check_mark: 유저 로그인 테스트 .http
    
    * :white_check_mark: add 테스트 환경 security config
    
    * :fire: 안쓰는 AuthMethodNotSupportedException 제거
    
    * :sparkles: oauth2추가로 gradle 수정
    
    * :sparkles: .gitignore add > jwt 암호값으로 쓸 priv.pub 파일 추가

---
+++ src/main/java/Fridge_Chef/team/security/rest/TokenController.java
@@ -0,0 +1,48 @@
+package Fridge_Chef.team.security.rest;
+
+import Fridge_Chef.team.exception.ApiException;
+import Fridge_Chef.team.exception.ErrorCode;
+import Fridge_Chef.team.security.JwtProvider;
+import Fridge_Chef.team.security.rest.response.TokenRefreshResponse;
+import Fridge_Chef.team.user.domain.User;
+import Fridge_Chef.team.user.rest.model.AuthenticatedUser;
+import Fridge_Chef.team.user.rest.response.UserResponse;
+import Fridge_Chef.team.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class TokenController {
+    private final UserService userService;
+    private final JwtProvider jwtProvider;
+
+
+    @GetMapping("/access")
+    public UserResponse accessLogin(@AuthenticationPrincipal AuthenticatedUser userId) {
+        User user = userService.findByUserId(userId)
+                .orElseThrow(() -> new ApiException(ErrorCode.TOKEN_ACCESS_NOT_USER));
+        return createUserResponse(user);
+    }
+
+    @GetMapping("/refresh")
+    public TokenRefreshResponse isValidToken(@AuthenticationPrincipal AuthenticatedUser userId) {
+        User user = userService.findByUserId(userId)
+                .orElseThrow(() -> new ApiException(ErrorCode.TOKEN_ACCESS_NOT_USER));
+        return createUserRefreshTokenResponse(user);
+    }
+
+    private UserResponse createUserResponse(User user) {
+        String token = jwtProvider.create(user.getUserId());
+        return UserResponse.from(user, token);
+    }
+
+    private TokenRefreshResponse createUserRefreshTokenResponse(User user) {
+        String token = jwtProvider.createRefreshToken(user.getUserId());
+        return TokenRefreshResponse.from(token);
+    }
+}
