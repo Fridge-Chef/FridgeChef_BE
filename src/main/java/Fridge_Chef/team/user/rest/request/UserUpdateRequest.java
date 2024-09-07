c1a696abe2d2c54e7c1851fe0b5696269261727f
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-08 02:26:42 +0900

    :sparkles: 유저 토큰 로그인 회원가입  api  req 추가

---
+++ src/main/java/Fridge_Chef/team/user/rest/request/UserUpdateRequest.java
@@ -0,0 +1,10 @@
+package Fridge_Chef.team.user.rest.request;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+    private String username;
+    private String email;
+    private String password;
+}
