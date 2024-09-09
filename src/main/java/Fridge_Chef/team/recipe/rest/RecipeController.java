5a72350adff87c0a464026376e694e3a6e2a359e
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-09 14:10:13 +0900

    :fire: 테스트용 레시피 제거

--- src/main/java/Fridge_Chef/team/recipe/rest/RecipeController.java
+++ src/main/java/Fridge_Chef/team/recipe/rest/RecipeController.java
@@ -1,26 +1,11 @@
 package Fridge_Chef.team.recipe.rest;
 
-import Fridge_Chef.team.recipe.rest.request.SampleRequest;
-import Fridge_Chef.team.recipe.rest.response.SampleResponse;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/recipe")
 @RequiredArgsConstructor
 public class RecipeController {
-
-    @GetMapping()
-    public String info(){
-        return "web request success";
-    }
-
-    @PostMapping("/test")
-    public SampleResponse docs(@RequestBody SampleRequest dto){
-        return new SampleResponse(dto.getName(),dto.getValue());
-    }
-
 }
