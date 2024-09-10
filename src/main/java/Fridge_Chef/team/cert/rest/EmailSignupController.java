5b43c58e9431f20742037136ef51bf318867f077
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-10 20:38:57 +0900

    :sparkles: 이메일인증 api 추가

--- src/main/java/Fridge_Chef/team/cert/rest/EmailSignupController.java
+++ src/main/java/Fridge_Chef/team/cert/rest/EmailSignupController.java
@@ -7,46 +7,30 @@ import Fridge_Chef.team.cert.service.request.SignUpCertRequest;
 import Fridge_Chef.team.cert.service.request.SignUpEmailVerifyRequest;
 import Fridge_Chef.team.mail.service.EmailService;
 import Fridge_Chef.team.cert.rest.request.SignUpEmailSendRequest;
-import Fridge_Chef.team.cert.rest.request.SignUpRequest;
 import Fridge_Chef.team.cert.rest.response.SignUpCertVerifyResponse;
-import Fridge_Chef.team.cert.rest.response.SignUpIdCheckResponse;
 import Fridge_Chef.team.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user/signup")
+@RequestMapping("/api/user/email")
 @RequiredArgsConstructor
-public class SignUpController {
+public class EmailSignupController {
     private final CertService certService;
     private final UserService userService;
     private final EmailService emailService;
 
-    @PostMapping
-    public void signup(@Valid @RequestBody SignUpRequest request) {
-        certService.validateCert(request.email());
-        userService.validateMemberRegistration(request.userId(), request.email());
-        userService.signup(request.email(),request.password(), request.userName());
-        certService.deleteAuthenticationComplete(request);
-    }
 
-    @PostMapping("/idCheck")
-    public SignUpIdCheckResponse idCheck(@RequestBody String id) {
-        return new SignUpIdCheckResponse(userService.isIdCheck(id));
-    }
-
-
-    @PostMapping("/email/send")
+    @PostMapping("/send")
     public void emailCertCodeSend(@Valid @RequestBody SignUpEmailSendRequest request) {
         userService.checkEmailValidAndUnique(request.email());
-        certService.validateEmailVerificationExceed(request.email());
         int verificationCode = certService.newVerificationCode();
         emailService.signupCertSend(request.email(), verificationCode);
         certService.saveCert(new SignUpCertRequest(verificationCode, request.email()));
     }
 
-    @PostMapping ("/email/verify")
+    @PostMapping("/verify")
     public SignUpCertVerifyResponse emailCertVerify(@Valid @RequestBody SignUpEmailVerifyRequest verifyRequest) {
         return certService.emailVerifyCodeCheck(EmailVerifyRequest.of(verifyRequest.send(), verifyRequest.code()));
     }
