13ff66c7285ed8c988b4274d5668bbf5931bf815
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-11 14:31:53 +0900

    :sparkles: 비밀번호 공통 @Valid 추가

--- src/main/java/Fridge_Chef/team/common/validator/PasswordValid.java
+++ src/main/java/Fridge_Chef/team/common/validator/PasswordValid.java
@@ -1,16 +1,16 @@
 package Fridge_Chef.team.common.validator;
 
 
+import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
-@Retention(RUNTIME)
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PasswordValid {
 
     String message() default "비밀번호: 6~38자의 영문, 숫자를 사용해주세요.";
@@ -18,4 +18,5 @@ public @interface PasswordValid {
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
+
 }
