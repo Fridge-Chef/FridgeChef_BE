aacc896e5e6359c6d05e4709ee4508c0259b3e7a
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-20 16:12:21 +0900

    :bug: star 추가

---
+++ src/main/java/Fridge_Chef/team/common/entity/Star.java
@@ -0,0 +1,17 @@
+package Fridge_Chef.team.common.entity;
+
+public enum Star {
+
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5);
+
+    private final int value;
+
+    Star(int value) {
+        this.value = value;
+    }
+
+}
