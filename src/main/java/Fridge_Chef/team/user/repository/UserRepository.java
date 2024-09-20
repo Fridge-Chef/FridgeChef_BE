420da71552b9c4cd42627135717c8ce1113537a0
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-20 16:39:18 +0900

    :recycle: 유저 조회 추가

--- src/main/java/Fridge_Chef/team/user/repository/UserRepository.java
+++ src/main/java/Fridge_Chef/team/user/repository/UserRepository.java
@@ -1,16 +1,24 @@
 package Fridge_Chef.team.user.repository;
 
 
+import Fridge_Chef.team.common.entity.OracleBoolean;
 import Fridge_Chef.team.user.domain.User;
+import Fridge_Chef.team.user.domain.UserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId_Value(UUID userId);
+
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    void deleteByUserId(UserId userId);
+
+    List<User> findAllByIsDelete(OracleBoolean isDelete);
 }
