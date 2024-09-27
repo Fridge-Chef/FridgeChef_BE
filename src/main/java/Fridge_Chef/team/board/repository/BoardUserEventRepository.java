b94c723cb630b0ce453be9eea1cde8adeb5ef1f2
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-27 17:34:45 +0900

    :sparkles: add boards repository
    Signed-off-by: JHKoder <jeonghun.kang.dev@gmail.com>

--- src/main/java/Fridge_Chef/team/board/repository/BoardUserEventRepository.java
+++ src/main/java/Fridge_Chef/team/board/repository/BoardUserEventRepository.java
@@ -1,13 +1,12 @@
 package Fridge_Chef.team.board.repository;
 
 import Fridge_Chef.team.board.domain.Board;
-import Fridge_Chef.team.user.domain.UserId;
+import Fridge_Chef.team.board.domain.BoardUserEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
-    Optional<List<Board>> findByUserId(UserId userId);
+public interface BoardUserEventRepository extends JpaRepository<BoardUserEvent,Long> {
+    List<BoardUserEvent> findByBoard(Board board);
 }
