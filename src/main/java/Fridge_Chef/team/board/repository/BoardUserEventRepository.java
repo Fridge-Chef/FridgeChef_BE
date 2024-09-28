fc26b0e247c19d6ae9e372133a83d52932cc68d8
JHKoder <jeonghun.kang.dev@gmail.com>
2024-09-28 12:35:11 +0900

    :art: code change
    Signed-off-by: JHKoder <jeonghun.kang.dev@gmail.com>

--- src/main/java/Fridge_Chef/team/board/repository/BoardUserEventRepository.java
+++ src/main/java/Fridge_Chef/team/board/repository/BoardUserEventRepository.java
@@ -2,11 +2,13 @@ package Fridge_Chef.team.board.repository;
 
 import Fridge_Chef.team.board.domain.Board;
 import Fridge_Chef.team.board.domain.BoardUserEvent;
+import Fridge_Chef.team.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface BoardUserEventRepository extends JpaRepository<BoardUserEvent,Long> {
+public interface BoardUserEventRepository extends JpaRepository<BoardUserEvent, Long> {
     List<BoardUserEvent> findByBoard(Board board);
+
+    List<BoardUserEvent> findByBoardAndUser(Board board, User user);
 }
