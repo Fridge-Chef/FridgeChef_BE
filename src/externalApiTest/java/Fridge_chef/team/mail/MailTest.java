5f6f02582fd1c71191bf5e6f7046c3b5c49d3a5e
Kang JeoungHun <jeonghun.kang.dev@gmail.com>
2024-10-01 10:47:51 +0900

    ✅  메일 실제 테스트 추가  (#139)
    * :white_check_mark: 메일 실제 기능 동작 테스트
    
    Signed-off-by: JHKoder <jeonghun.kang.dev@gmail.com>
    
    * :recycle: 메일 주소
    
    Signed-off-by: JHKoder <jeonghun.kang.dev@gmail.com>
    
    ---------
    
    Signed-off-by: JHKoder <jeonghun.kang.dev@gmail.com>

--- src/externalApiTest/java/Fridge_chef/team/mail/MailTest.java
+++ src/externalApiTest/java/Fridge_chef/team/mail/MailTest.java
@@ -1,4 +1,25 @@
 package Fridge_chef.team.mail;
 
-public class MailTest {
+import Fridge_Chef.team.mail.service.EmailService;
+import Fridge_Chef.team.mail.service.request.EmailRequest;
+import Fridge_chef.team.FridgeChefApplicationApiTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+@DisplayName("mail 테스트")
+public class MailTest extends FridgeChefApplicationApiTest {
+    @Autowired
+    private EmailService emailService;
+
+    @Value("${spring.mail.username}")
+    private String testEmail;
+
+    @Test
+    @DisplayName("전송 성공")
+    void send(){
+        Assertions.assertDoesNotThrow(() -> emailService.sendMessage(new EmailRequest(testEmail, "test 용 이메일", "test")));
+    }
 }
