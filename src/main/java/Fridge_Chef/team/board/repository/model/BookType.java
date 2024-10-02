203d199583f1be286049360b94093dd6b10d0237
JHKoder <jeonghun.kang.dev@gmail.com>
2024-10-02 22:22:41 +0900

    :sparkles: 북 타입 추가
    Signed-off-by: JHKoder <jeonghun.kang.dev@gmail.com>

---
+++ src/main/java/Fridge_Chef/team/board/repository/model/BookType.java
@@ -0,0 +1,15 @@
+package Fridge_Chef.team.board.repository.model;
+
+import lombok.Getter;
+
+@Getter
+public enum BookType {
+    LIKE("찜하기"),
+    MYRECIPE("나만의 레시피"),
+    COMMENT("레시피 후기");
+    private final String name;
+
+    BookType(String name) {
+        this.name = name;
+    }
+}
