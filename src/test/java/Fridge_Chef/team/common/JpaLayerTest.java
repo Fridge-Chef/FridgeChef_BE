5f2d2b3318702a31d28b5a192a4de5746a6b34bf
JHKoder <jeonghun.kang.dev@gmail.com>
2024-10-03 15:30:10 +0900

    :recycle: 기타 수정
    Signed-off-by: JHKoder <jeonghun.kang.dev@gmail.com>

--- src/test/java/Fridge_Chef/team/common/JpaLayerTest.java
+++
@@ -1,7 +0,0 @@
-package Fridge_Chef.team.common;
-
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-public class JpaLayerTest {
-}
--- src/test/java/Fridge_Chef/team/user/repository/UserRepositoryTest.java
+++ src/test/java/Fridge_Chef/team/user/repository/UserRepositoryTest.java
@@ -1,6 +1,6 @@
 package Fridge_Chef.team.user.repository;
 
-import Fridge_Chef.team.common.JpaLayerTest;
+import Fridge_Chef.team.common.QueryDslTest;
 
-public class UserRepositoryTest extends JpaLayerTest {
+public class UserRepositoryTest extends QueryDslTest {
 }
