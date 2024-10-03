3352a235fca429b65cee25917641f8af1570b77f
ABCganada <shinmg9712@gmail.com>
2024-10-03 22:36:27 +0900

    feat : 레시피 db 전체 저장 로직

---
+++ src/main/java/Fridge_Chef/team/recipe/dump/DumpController.java
@@ -0,0 +1,20 @@
+package Fridge_Chef.team.recipe.dump;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dump")
+@RequiredArgsConstructor
+public class DumpController {
+
+    private final DumpService dumpService;
+
+    @PostMapping("/")
+    public void dump() {
+
+        dumpService.insertAll();
+    }
+}
