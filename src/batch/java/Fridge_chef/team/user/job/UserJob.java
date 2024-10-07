91ebad6eedbd2327408e3f14c8182b9e6b99118e
JHKoder <jeonghun.kang.dev@gmail.com>
2024-10-07 18:07:14 +0900

    :sparkles: 유저 삭제 정책 추가

--- src/batch/java/Fridge_chef/team/user/job/UserJob.java
+++ src/batch/java/Fridge_chef/team/user/job/UserJob.java
@@ -1,4 +1,26 @@
 package Fridge_chef.team.user.job;
 
+import Fridge_Chef.team.common.entity.OracleBoolean;
+import Fridge_Chef.team.user.domain.User;
+import Fridge_Chef.team.user.repository.UserRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class UserJob {
+    private final UserRepository userRepository;
+
+    public UserJob(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 2 * * ?")
+    void userDeletePolicy(){
+        List<User> users= userRepository.findByDelete(OracleBoolean.T);
+        userRepository.deleteAll(users);
+    }
 }
