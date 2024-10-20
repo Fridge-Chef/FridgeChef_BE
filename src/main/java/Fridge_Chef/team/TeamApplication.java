bc4d88539e9273696e52ca544a3085a19f2af6d4
JHKoder <jeonghun.kang.dev@gmail.com>
2024-10-20 23:27:44 +0900

    :sparkles: add page config support - spring boot 3.2 update Page<?> return dto

--- src/main/java/Fridge_Chef/team/TeamApplication.java
+++ src/main/java/Fridge_Chef/team/TeamApplication.java
@@ -3,11 +3,15 @@ package Fridge_Chef.team;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.retry.annotation.EnableRetry;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @EnableRetry
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class TeamApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeamApplication.class, args);
