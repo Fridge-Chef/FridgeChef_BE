24af857f46d29d314ed203b380ce4851ca12893f
ABCganada <shinmg9712@gmail.com>
2024-10-26 18:12:50 +0900

    fix : context 저장 수정

--- src/main/java/Fridge_Chef/team/recipe/dump/BasicRecipesInitializer.java
+++ src/main/java/Fridge_Chef/team/recipe/dump/BasicRecipesInitializer.java
@@ -209,10 +209,12 @@ public class BasicRecipesInitializer {
         recipeIngredientRepository.saveAll(recipe.getRecipeIngredients());
     }
 
+    @Transactional
     private void recipeToBoard(User user, Recipe recipe) {
 
-        Context context = Context.formMyUserRecipe(recipe.getRecipeIngredients(), recipe.getDescriptions());
-        context = contextRepository.save(context);
+        Context context = Context.formMyUserRecipe(String.valueOf(recipe.getCookTime()), String.valueOf(recipe.getDifficult()), recipe.getCategory(),
+                recipe.getRecipeIngredients(), recipe.getDescriptions());
+        contextRepository.save(context);
 
         Board board = Board.from(user, recipe);
         board.setContext(context);
