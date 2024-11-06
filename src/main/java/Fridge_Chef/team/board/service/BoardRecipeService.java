c1fd746c60acb2a2f133690fa8ebda6798079ede
JHKoder <jeonghun.kang.dev@gmail.com>
2024-11-06 18:40:43 +0900

    :recycle: 게시판 서비스 생성,수정

--- src/main/java/Fridge_Chef/team/board/service/BoardRecipeService.java
+++ src/main/java/Fridge_Chef/team/board/service/BoardRecipeService.java
@@ -9,7 +9,9 @@ import Fridge_Chef.team.board.rest.request.BoardByRecipeUpdateRequest;
 import Fridge_Chef.team.exception.ApiException;
 import Fridge_Chef.team.exception.ErrorCode;
 import Fridge_Chef.team.image.domain.Image;
+import Fridge_Chef.team.recipe.domain.Recipe;
 import Fridge_Chef.team.recipe.domain.RecipeIngredient;
+import Fridge_Chef.team.recipe.repository.RecipeRepository;
 import Fridge_Chef.team.user.domain.User;
 import Fridge_Chef.team.user.domain.UserId;
 import Fridge_Chef.team.user.repository.UserRepository;
@@ -26,6 +28,7 @@ public class BoardRecipeService {
     private final BoardRepository boardRepository;
     private final BoardUserEventRepository boardUserEventRepository;
     private final ContextRepository contextRepository;
+    private final RecipeRepository recipeRepository;
 
     @Transactional
     public Board create(UserId userId, BoardByRecipeRequest request,
@@ -36,13 +39,14 @@ public class BoardRecipeService {
 
         Context context = contextRepository.save(
                 Context.formMyUserRecipe(
-                        request.getDishTime(),request.getDishLevel(),request.getDishCategory(),
+                        request.getDishTime(), request.getDishLevel(), request.getDishCategory(),
                         recipeIngredient, descriptions)
         );
 
-        Board board = boardRepository.save(new Board(user, request.getDescription(),request.getName(), context, image, BoardType.USER));
+        Board board = boardRepository.save(new Board(user, request.getDescription(), request.getName(), context, image, BoardType.USER));
         BoardUserEvent event = new BoardUserEvent(board, user);
         boardUserEventRepository.save(event);
+        recipeRepository.save(Recipe.ofBoard(board));
         return board;
     }
 
@@ -53,17 +57,21 @@ public class BoardRecipeService {
         findByUserId(userId);
         Board board = boardRepository.findById(request.getId())
                 .orElseThrow(() -> new ApiException(ErrorCode.BOARD_NOT_FOUND));
+        validBoardUser(board,userId);
 
         board.updateMainImage(mainImage);
         board.updateTitle(request.getTitle());
 
         Context context = board.getContext();
-        context.updateIngredients(ingredients);
-        context.updateDescriptions(descriptions);
-        context.updateDish(request.getDishTime(),request.getDishLevel(),request.getDishCategory());
+        context.update(ingredients,descriptions,request.getDishTime(),request.getDishLevel(),request.getDishCategory());
 
         return board;
+    }
 
+    private void validBoardUser(Board board, UserId userId) {
+        if(!board.getUser().getUserId().equals(userId)){
+            throw new ApiException(ErrorCode.BOARD_NOT_USER_CREATE);
+        }
     }
 
     private User findByUserId(UserId userId) {
