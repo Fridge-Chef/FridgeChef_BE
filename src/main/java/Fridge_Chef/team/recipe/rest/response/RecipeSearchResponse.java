8f5c79e2181c451c42ea1d345a3def356b88eeb0
JHKoder <jeonghun.kang.dev@gmail.com>
2024-11-07 16:53:18 +0900

    :sparkles: 추천 레시피 검색 response 변경

--- src/main/java/Fridge_Chef/team/recipe/rest/response/RecipeSearchResponse.java
+++ src/main/java/Fridge_Chef/team/recipe/rest/response/RecipeSearchResponse.java
@@ -1,10 +1,14 @@
 package Fridge_Chef.team.recipe.rest.response;
 
+import Fridge_Chef.team.board.domain.Board;
+import Fridge_Chef.team.recipe.domain.RecipeIngredient;
+import Fridge_Chef.team.user.domain.UserId;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -13,12 +17,40 @@ import java.util.List;
 @AllArgsConstructor
 public class RecipeSearchResponse {
 
-    private String name;
-    private String imageUrl;
-    private double totalStar;
+    private Long id;
+    private String title;
+    private String userName;
+    private String mainImage;
+    private double star;
     private int hit;
-    private int totalIngredients;
-    private int have;
+    private boolean myHit;
+    private int click;
+    private LocalDateTime createTime;
+
+    private long have;
+    private long withoutCount;
     private List<String> without;
-    private Long boardId;
+
+    public static RecipeSearchResponse of(Board board, List<String> pick) {
+        List<RecipeIngredient> ingredients = board.getContext().getBoardIngredients();
+        List<String> without = ingredients.stream()
+                .map(pi -> pi.getIngredient().getName())
+                .filter(ingredientName -> !pick.contains(ingredientName))
+                .toList();
+
+        return new RecipeSearchResponse(board.getId(),
+                board.getTitle(),
+                board.getUser().getUsername(),
+                board.getMainImageLink(),
+                board.getTotalStar(),
+                board.getHit(),
+                board.getIsMyHit(UserId.create()),
+                board.getCount(),
+                board.getCreateTime(),
+                ingredients.size() - without.size(),
+                without.size(),
+                without
+        );
+    }
+
 }
