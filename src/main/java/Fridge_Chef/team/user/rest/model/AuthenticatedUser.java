0b5b8ee587fefacc0b91dd8faf241a9c07586535
JHKoder <jeonghun.kang.dev@gmail.com>
2024-11-07 17:14:05 +0900

    :recycle: 비유저 Optional 사용 하여 감별

--- src/main/java/Fridge_Chef/team/user/rest/model/AuthenticatedUser.java
+++ src/main/java/Fridge_Chef/team/user/rest/model/AuthenticatedUser.java
@@ -2,5 +2,11 @@ package Fridge_Chef.team.user.rest.model;
 
 import Fridge_Chef.team.user.domain.Role;
 import Fridge_Chef.team.user.domain.UserId;
+
+import java.util.Optional;
+
 public record AuthenticatedUser(UserId userId, Role role) {
+    public static Optional<UserId> anonymousUser(AuthenticatedUser user){
+        return user == null ? Optional.empty(): Optional.of(user.userId());
+    }
 }
