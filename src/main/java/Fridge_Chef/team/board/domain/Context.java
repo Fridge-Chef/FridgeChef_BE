7cea518c8a9bc962fc2152c7f09713ae7ab7ed23
JHKoder <jeonghun.kang.dev@gmail.com>
2024-11-13 04:15:57 +0900

    :recycle: Context Entity to Embeddable type change

--- src/main/java/Fridge_Chef/team/board/domain/Context.java
+++ src/main/java/Fridge_Chef/team/board/domain/Context.java
@@ -2,34 +2,26 @@ package Fridge_Chef.team.board.domain;
 
 import Fridge_Chef.team.recipe.domain.RecipeIngredient;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static lombok.AccessLevel.PROTECTED;
-
-@Entity
 @Getter
-@Table
-@NoArgsConstructor(access = PROTECTED)
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
 public class Context {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
-    @OneToOne(fetch = FetchType.LAZY)
-    private Board board;
     private String dishTime;
     private String dishLevel;
     private String dishCategory;
-
     @Column(length = 500)
     private String pathIngredient;
-    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.PERSIST)
     private List<RecipeIngredient> boardIngredients;
-    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.PERSIST)
     private List<Description> descriptions;
 
     private Context(String dishTime, String dishLevel, String dishCategory, List<RecipeIngredient> boardIngredients, List<Description> descriptions) {
@@ -78,18 +70,10 @@ public class Context {
         this.dishCategory = dishCategory;
     }
 
-
     public void slicePathIngredient() {
         pathIngredient = boardIngredients.stream()
                 .map(ingredient -> ingredient.getIngredient().getName())
                 .collect(Collectors.joining(","));
-    }
-
-    public void removeAllIngredient() {
-    }
-
-    public void removeRecipeIngredient(RecipeIngredient boardIngredient) {
-        this.boardIngredients.remove(boardIngredient);
     }
 
     public void addRecipeIngredient(RecipeIngredient recipeIngredient) {
