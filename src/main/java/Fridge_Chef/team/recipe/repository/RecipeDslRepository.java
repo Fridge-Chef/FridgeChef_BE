2ecab6b18dd8eaf9227bcd7d23aa5e7edee7c031
JHKoder <jeonghun.kang.dev@gmail.com>
2024-11-13 04:18:46 +0900

    :recycle: 레시피 검색 변경 리펙토링 n+1 해결

--- src/main/java/Fridge_Chef/team/recipe/repository/RecipeDslRepository.java
+++ src/main/java/Fridge_Chef/team/recipe/repository/RecipeDslRepository.java
@@ -1,11 +1,6 @@
 package Fridge_Chef.team.recipe.repository;
 
 import Fridge_Chef.team.board.domain.Board;
-import Fridge_Chef.team.board.domain.Context;
-import Fridge_Chef.team.board.repository.BoardRepository;
-import Fridge_Chef.team.exception.ApiException;
-import Fridge_Chef.team.exception.ErrorCode;
-import Fridge_Chef.team.recipe.domain.Recipe;
 import Fridge_Chef.team.recipe.repository.model.RecipeSearchSortType;
 import Fridge_Chef.team.recipe.rest.request.RecipePageRequest;
 import Fridge_Chef.team.recipe.rest.response.RecipeSearchResponse;
@@ -24,25 +19,24 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static Fridge_Chef.team.board.domain.QBoard.board;
-import static Fridge_Chef.team.image.domain.QImage.image;
 import static Fridge_Chef.team.ingredient.domain.QIngredient.ingredient;
-import static Fridge_Chef.team.recipe.domain.QRecipe.recipe;
 import static Fridge_Chef.team.recipe.domain.QRecipeIngredient.recipeIngredient;
 
 /**
  * 100개 레시피 단어 검색 [ 성능 최적화 기록 ]
  * 기존 단어만 검색시 Time: 1262ms (1 s 262 ms) ~ 1294ms (1 s 294 ms);
+ * <p>
+ * 이슈 : n+1 문제
+ * n+1 문제 해결후 0.039ms 으로 해결
+
  */
 @Slf4j
 @Repository
 @RequiredArgsConstructor
 public class RecipeDslRepository {
-
     private final JPAQueryFactory factory;
-    private final BoardRepository boardRepository;
 
 
     @Transactional(readOnly = true)
@@ -53,28 +47,27 @@ public class RecipeDslRepository {
                 .limit(pageable.getPageSize())
                 .leftJoin(board.context.boardIngredients, recipeIngredient)
                 .leftJoin(recipeIngredient.ingredient, ingredient)
-                .groupBy(board);
+                .groupBy(board, recipeIngredient, ingredient, ingredient.name);
 
-        List<String> pick = new ArrayList<>(ingredients);
-        pick.addAll(must);
+        List<String> pick = merge(must,ingredients);
+
         BooleanBuilder builder = new BooleanBuilder();
-
         if (!must.isEmpty()) {
             for (String mu : must) {
                 builder.and(recipeIngredient.ingredient.name.eq(mu));
             }
         }
-        builder.and(recipeIngredient.ingredient.name.in(pick));
-
-        long totalPage = query.fetch().size();
+        builder.and(ingredient.name.in(pick));
         query.where(builder);
+        int totalPage = query.fetch().size();
 
         applySort(query, request.getSortType());
 
         List<RecipeSearchResponse> responses = query.fetch()
                 .stream()
-                .map(value -> RecipeSearchResponse.of(value, mergeList(must, ingredients), userId))
+                .map(value -> RecipeSearchResponse.of(value, pick, userId))
                 .toList();
+
         return PageableExecutionUtils.getPage(responses, pageable, () -> totalPage);
     }
 
@@ -87,90 +80,7 @@ public class RecipeDslRepository {
         }
     }
 
-    @Transactional(readOnly = true)
-    public Page<RecipeSearchResponse> findRecipesByIngredient(PageRequest pageable, RecipePageRequest request, List<String> must, List<String> ingredients) {
-
-        JPAQuery<Recipe> query = createBaseQuery(must, ingredients);
-
-        List<RecipeSearchResponse> recipes = query
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch()
-                .stream()
-                .map(recipe -> convertToRecipeSearchResponse(recipe, must, ingredients))
-                .toList();
-
-        List<RecipeSearchResponse> sortedRecipes = applySort(recipes, request.getSortType());
-
-        return PageableExecutionUtils.getPage(sortedRecipes, pageable, () -> query.fetch().size());
-    }
-
-    private JPAQuery<Recipe> createBaseQuery(List<String> must, List<String> ingredients) {
-        JPAQuery<Recipe> query = factory
-                .selectFrom(recipe)
-                .leftJoin(recipe.recipeIngredients, recipeIngredient)
-                .leftJoin(recipe.image, image)
-                .groupBy(recipe.id, recipe.category, recipe.cookTime, recipe.difficult, recipe.createTime,
-                        recipe.deleteStatus, recipe.image.id, recipe.intro, recipe.name, recipe.updateTime);
-
-        if (must != null && !must.isEmpty()) {
-            query.where(recipeIngredient.ingredient.name.in(must))
-                    .having(recipeIngredient.ingredient.name.countDistinct().goe(must.size()));
-        }
-
-        if (ingredients != null && !ingredients.isEmpty()) {
-            query.where(recipeIngredient.ingredient.name.in(ingredients).or(recipeIngredient.ingredient.name.in(must)));
-        }
-
-        return query;
-    }
-
-    private RecipeSearchResponse convertToRecipeSearchResponse(Recipe recipe, List<String> must, List<String> ingredients) {
-
-        List<String> recipeIngredientNames = recipe.getRecipeIngredients().stream()
-                .map(ri -> ri.getIngredient().getName())
-                .toList();
-
-        List<String> without = recipeIngredientNames.stream()
-                .filter(ri -> !ingredients.contains(ri) && !must.contains(ri))
-                .collect(Collectors.toList());
-
-        int have = recipeIngredientNames.size() - without.size();
-        String imageUrl = recipe.getImage() != null ? recipe.getImage().getLink() : "";
-
-        Board board = findBoardFromRecipe(recipe);
-
-        if (board == null) {
-            throw new ApiException(ErrorCode.BOARD_NOT_FOUND);
-        }
-
-        return null;
-    }
-
-    private List<RecipeSearchResponse> applySort(List<RecipeSearchResponse> recipes, RecipeSearchSortType sortType) {
-        List<RecipeSearchResponse> sortedRecipes = new ArrayList<>(recipes);
-        return sortedRecipes;
-    }
-
-    private Board findBoardFromRecipe(Recipe recipe) {
-
-        List<Board> boardList = boardRepository.findByTitle(recipe.getName())
-                .orElseThrow(() -> new ApiException(ErrorCode.BOARD_NOT_FOUND));
-
-        for (Board board : boardList) {
-            Context context = board.getContext();
-
-            if (context.getDishCategory().equals(recipe.getCategory())
-                    && context.getDishLevel().equals(String.valueOf(recipe.getDifficult()))
-                    && context.getDishTime().equals(String.valueOf(recipe.getCookTime()))) {
-                return board;
-            }
-        }
-
-        return null;
-    }
-
-    private List<String> mergeList(List<String> left, List<String> right) {
+    private List<String> merge(List<String> left, List<String> right) {
         List<String> list = new ArrayList<>(left);
         list.addAll(right);
         return list;
