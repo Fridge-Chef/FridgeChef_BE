531370054622fe668746e3c106c48d65b51217ef
JHKoder <jeonghun.kang.dev@gmail.com>
2024-12-30 22:20:20 +0900

    :recycle: board req,res 수정

--- src/main/java/Fridge_Chef/team/board/service/response/BoardMyRecipePageResponse.java
+++ src/main/java/Fridge_Chef/team/board/service/response/BoardMyRecipePageResponse.java
@@ -3,26 +3,11 @@ package Fridge_Chef.team.board.service.response;
 import Fridge_Chef.team.board.domain.Board;
 import Fridge_Chef.team.board.repository.model.SortType;
 import Fridge_Chef.team.user.domain.UserId;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-public class BoardMyRecipePageResponse {
-    private SortType sortType;
-    private Long boardId;
-    private String title;
-    private String userName;
-    private String mainImage;
-    private Long mainImageId;
-    private double star;
-    private int hit;
-    private boolean myHit;
-    private boolean myMe;
-    private int click;
-    private LocalDateTime createTime;
+public record BoardMyRecipePageResponse(SortType sortType, Long boardId, String title, String userName, String mainImage, Long mainImageId, double star, int hit, boolean myHit, boolean myMe, int click, LocalDateTime createTime) {
+
 
     public static BoardMyRecipePageResponse ofEntity(SortType sortType, Board entity, UserId userId) {
         String link = "";
